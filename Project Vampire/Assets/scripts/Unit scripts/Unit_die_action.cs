3568376b4f282bf127fa55e87669c9981f9e63a0
xinhe <u7518549@anu.edu.au>
2023-09-29 15:14:35 +1000

    working on week 8, including import free asset from unity store, modularizing the units scripts and processing the imported model animation

--- Project Vampire/Assets/scripts/Unit scripts/Unit_die_action.cs
+++ Project Vampire/Assets/scripts/Unit scripts/Unit_die_action.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class Unit_die_action : MonoBehaviour
 {
     private Unit_attributes _attributes;
+    private Animator _animator;
     private float _HP;
+    private bool _isDead;
     // Start is called before the first frame update
     void Start()
     {
         _attributes = GetComponent<Unit_attributes>();
-        _HP = _attributes.HP;
+        _animator = GetComponent<Animator>();
+        _isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_HP <= 0)
+        _HP = _attributes.HP;
+        if (_HP <= 0 && !_isDead)
         {
-            Destroy(gameObject);
+            _isDead = true;
+            _animator.SetTrigger("isDead");
         }
+    }
+    public void OnDeathAnimationEnd()
+    {
+        Destroy(gameObject);
     }
 }
