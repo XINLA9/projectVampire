e09fd8525b86b787d2de5935cad6b7e7dc207117
Taowen Huang <u7147693@anu.edu.au>
2023-10-04 21:44:33 +1100

    Finish shooting basic movement

--- Project Vampire/Assets/scripts/Unit scripts/Unit_shooting_action.cs
+++ Project Vampire/Assets/scripts/Unit scripts/Unit_shooting_action.cs
@@ -8,17 +8,20 @@ public class ShootingBullet : MonoBehaviour
     // Initiate the movement speed and attackRange
     public float speed = 10f;
     public float attackRange = 10.0f;
+    public float rotationSpeed = 20.0f;
     public GameObject nearestEnemy;
     public LayerMask enemiesToShoot;
     public bool shootCoolDown = true;
     private Rigidbody enemyRb;
     public Vector3 moveAway;
+    private Animator shooterAnim; 
 
     // Start is called before the first frame update
     void Start()
     {
         // Assign the rigidBody of enemy to variable
         enemyRb = GetComponent<Rigidbody>();
+        shooterAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -45,6 +48,8 @@ public class ShootingBullet : MonoBehaviour
             }
         } else {
             nearestEnemy = null;
+            shooterAnim.ResetTrigger("Enemy_detected");
+            enemyRb.velocity = Vector3.zero;
         }
     }
 
@@ -52,10 +57,15 @@ public class ShootingBullet : MonoBehaviour
         if (nearestEnemy != null) {
             moveAway = (transform.position - nearestEnemy.transform.position).normalized;
             Vector3 shootDir = (nearestEnemy.transform.position - transform.position).normalized;
-            enemyRb.AddForce(moveAway  * speed);
+            Quaternion toRotation = Quaternion.LookRotation(shootDir, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            shooterAnim.SetTrigger("Enemy_detected");
+            enemyRb.AddForce(moveAway * speed);
             if (shootCoolDown) {
+                shooterAnim.SetTrigger("Ready_fire");
                 var newBullet = Instantiate(bullet, GameObject.Find("Fire_heart").transform.position, bullet.transform.rotation);
-                newBullet.GetComponent<Rigidbody>().AddForce(shootDir * speed);
+                Rigidbody rb = newBullet.GetComponent<Rigidbody>();
+                rb.AddForce(shootDir * speed, ForceMode.Impulse);
                 shootCoolDown = false;
                 StartCoroutine(ReadyToShoot());
             }
