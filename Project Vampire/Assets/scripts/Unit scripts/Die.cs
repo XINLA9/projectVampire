20a70469234806879f44c07b12f7953b90179ff6
Taowen Huang <u7147693@anu.edu.au>
2023-10-29 20:44:01 +1100

    Add HP bar

--- Project Vampire/Assets/scripts/Unit scripts/Die.cs
+++ Project Vampire/Assets/scripts/Unit scripts/Die.cs
@@ -42,14 +42,8 @@ public class Die : MonoBehaviour
                 SummonDead SD = GetComponent<SummonDead>();
                 SD.enabled = false;
             }
-            if (isBeholder) {
-                Instantiate(trappedCir, transform.position, trappedCir.transform.rotation);
-            }
-            if (isMushroom) {
-                Instantiate(speedUpCir, transform.position, speedUpCir.transform.rotation);
-            }
-            if (isDragon) {
-                Instantiate(poisonCir, transform.position, poisonCir.transform.rotation);
+            if (isBeholder || isSkeleton || isMushroom || isDragon) {
+                StartCoroutine(DestoryBody());
             }
             Collider BC = gameObject.GetComponent<Collider>();
             Rigidbody rb = gameObject.GetComponent<Rigidbody>();
@@ -65,15 +59,21 @@ public class Die : MonoBehaviour
             uiElement.SetActive(false);
             int LayerIgnoreRaycast = LayerMask.NameToLayer("Dead");
             gameObject.layer = LayerIgnoreRaycast;
-            if (isSkeleton) {
-                StartCoroutine(DestorySkeleton());
-            }
         }
     }
 
-    IEnumerator DestorySkeleton() {
+    IEnumerator DestoryBody() {
         yield return new WaitForSeconds(3);
         Destroy(gameObject);
+        if (isBeholder) {
+            Instantiate(trappedCir, transform.position, trappedCir.transform.rotation);
+        }
+        if (isMushroom) {
+            Instantiate(speedUpCir, transform.position, speedUpCir.transform.rotation);
+        }
+        if (isDragon) {
+            Instantiate(poisonCir, transform.position, poisonCir.transform.rotation);
+        }
     }
     public void OnDeathAnimationEnd()
     {
